b26d79b931fbab96f35618c3cf4cedf5baec99aa
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-17 17:03:22 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -11,14 +11,28 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Render the "home" layout for the main page and send the following msg
+//app.get('/', (req, res) => {
+//  res.render('home', { msg: 'Handlebars are Cool!' });
+//})
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
-})
+  res.render('events-index', { events: events });
+});
 
 // Tell our app to send the "hello world" message to our home page
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//app.get('/', (req, res) => {
+//  res.send('Hello World!')
+//})
+
+
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
