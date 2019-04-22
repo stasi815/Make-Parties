4d29d2e66a96fc9ee56ef95f31bcbb43d5c99f8c
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-22 11:57:57 -0700

    proper separation of concerns

--- controllers/events.js
+++ controllers/events.js
@@ -1,43 +1,12 @@
-// Initialize express
-const express = require('express')
-const methodOverride = require('method-override')
-const app = express()
+module.exports = function (app, models) {
 
-// require handlebars
-var exphbs = require('express-handlebars');
-
-// Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// Use handlebars to render
-app.set('view engine', 'handlebars');
-
-// Render the "home" layout for the main page and send the following msg
-//app.get('/', (req, res) => {
-//  res.render('home', { msg: 'Handlebars are Cool!' });
-//})
-
-// OUR MOCK ARRAY OF PROJECTS
-var events = [
-  { title: "January event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-  { title: "February event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-  { title: "March event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-]
-// INITIALIZE BODY-PARSER AND ADD IT TO APP
-const bodyParser = require('body-parser');
-
-const models = require('./db/models');
-
-require('./controllers/events')(app, models);
-
-// override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'));
-
-// The following line must appear AFTER const app = express() and before your routes!
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//const models = require('./db/models');
-
-//require('./controllers/events')(app, models);
+    // INDEX
+    app.get('/', (req, res) => {
+        models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+            res.render('events-index', { events: events });
+        });
+    });
+//};
 
 // CREATE
 app.post('/events', (req, res) => {
@@ -120,13 +89,6 @@ app.delete('/events/:id', (req, res) => {
     res.redirect(`/`);
   }).catch((err) => {
     console.log(err);
+    });
   });
-});
-
-// Choose a port to listen on
-const port = process.env.PORT || 3000;
-
-// Tell the app what port to listen on
-app.listen(port, () => {
-  console.log('App listening on port 3000!')
-})
+};
