0b616330378d81ede76c49a83f61578f65cd796e
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-22 18:18:04 -0700

    pushing heroku config info

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Initialize express
-const express = require('express')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
 
 // require handlebars
 var exphbs = require('express-handlebars');
@@ -38,90 +38,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //const models = require('./db/models');
 
 //require('./controllers/events')(app, models);
-
-// CREATE
-app.post('/events', (req, res) => {
-  models.Event.create(req.body).then(event => {
-    res.redirect(`/events/${event.id}`);
-  }).catch((err) => {
-    console.log(err)
-  });
-});
-
-//app.post('/events', (req, res) => {
-//  console.log(req.body);
-//})
-
-// INDEX
-
-//app.get('/', (req, res) => {
-//  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-//    res.render('events-index', { events: events });
-//  })
-//})
-//app.get('/', (req, res) => {
-//  models.Event.findAll().then(events => {
-//    res.render('events-index', { events: events });
-//  })
-//});
-
-//app.get('/', (req, res) => {
-//  res.render('events-index', { events: events });
-//});
-
-// NEW
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-});
-
-// Tell our app to send the "hello world" message to our home page
-//app.get('/', (req, res) => {
-//  res.send('Hello World!')
-//})
-
-// SHOW
-app.get('/events/:id', (req, res) => {
-  // Search for the event by its id that was passed in via req.params
-  models.Event.findByPk(req.params.id).then((event) => {
-    // If the id is for a valid event, show it
-    res.render('events-show', { event: event })
-  }).catch((err) => {
-    // if the id was for an event not in our db, log an error
-    console.log(err.message);
-  })
-});
-
-// EDIT
-app.get('/events/:id/edit', (req, res) => {
-  models.Event.findByPk(req.params.id).then((event) => {
-    res.render('events-edit', { event: event });
-  }).catch((err) => {
-    console.log(err.message);
-  })
-});
-
-// UPDATE
-app.put('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.update(req.body).then(event => {
-      res.redirect(`/events/${req.params.id}`);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-// DELETE
-app.delete('/events/:id', (req, res) => {
-  models.Event.findByPk(req.params.id).then(event => {
-    event.destroy();
-    res.redirect(`/`);
-  }).catch((err) => {
-    console.log(err);
-  });
-});
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
--- controllers/events.js
+++ controllers/events.js
@@ -1,11 +1,11 @@
 module.exports = function (app, models) {
 
-    // INDEX
-    app.get('/', (req, res) => {
-        models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
-            res.render('events-index', { events: events });
-        });
+// INDEX
+app.get('/', (req, res) => {
+    models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+        res.render('events-index', { events: events });
     });
+});
 //};
 
 // CREATE
