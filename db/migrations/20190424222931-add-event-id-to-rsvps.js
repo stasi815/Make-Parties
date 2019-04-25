a101869bb1c5ea007497dd7d819a9587b9eb85f6
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-25 16:22:44 -0700

    rsvp failure

--- db/migrations/20190424222931-add-event-id-to-rsvps.js
+++ db/migrations/20190424222931-add-event-id-to-rsvps.js
@@ -3,24 +3,25 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Rsvps', 'EventId', Sequelize.INTEGER).then(() => {
-    return queryInterface.addConstraint('Rsvps', ['EventId'], {
-      type: 'foreign key',
-      name: 'event_rsvps',
-      references: { //Required field
-        table: 'Events',
-        field: 'id'
-      },
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE'
+      return queryInterface.addConstraint('Rsvps', ['EventId'], {
+        type: 'foreign key',
+        name: 'event_rsvps',
+        references: { //Required field
+          table: 'Events',
+          field: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     });
-  });  /*
+  },  /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-  },
+
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Rsvps', 'EventId');
--- db/models/event.js
+++ db/models/event.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     desc: DataTypes.TEXT,
     imgUrl: DataTypes.STRING //add this line (don't forget the comma above!)
   }, {});
+
   Event.associate = function(models) {
     Event.hasMany(models.Rsvp);
     // associations can be defined here
