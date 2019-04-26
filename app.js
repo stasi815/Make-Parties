754964674c63cf6ca10170f70e07d7c693a35ac5
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-25 22:28:08 -0700

    rsvp working with db

--- app.js
+++ app.js
@@ -15,27 +15,23 @@ app.set('view engine', 'handlebars');
 //app.get('/', (req, res) => {
 //  res.render('home', { msg: 'Handlebars are Cool!' });
 //})
-
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+const models = require('./db/models');
 // OUR MOCK ARRAY OF PROJECTS
 var events = [
   { title: "January event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "February event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
   { title: "March event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
-// INITIALIZE BODY-PARSER AND ADD IT TO APP
-const bodyParser = require('body-parser');
-
-const models = require('./db/models');
-
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'));
 
-require('./controllers/events')(app, models);
-require('./controllers/rsvps')(app, models);
-
-
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./controllers/events')(app, models);
+require('./controllers/rsvps')(app, models);
 
 
 // Choose a port to listen on
