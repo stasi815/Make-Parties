27c02d2e253dba9fdb5ef0840e2e4112233671a0
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-25 22:59:44 -0700

    Users can delete rsvps

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -15,5 +15,13 @@ module.exports = (app, models) => {
           console.log(err)
       });
   });
-  // DESTROY
+  // DELETE
+  app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+      models.Rsvp.findByPk(req.params.id).then(rsvp => {
+          rsvp.destroy();
+          res.redirect(`/events/${req.params.eventId}`);
+        }).catch((err) => {
+          console.log(err);
+        });
+      });
 }
