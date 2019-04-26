b10e9cc918f40b1b45fe0bbd5dda15209dcba66b
DUC Mobile Mac Lab <ducmobilemaclab@DUCs-MacBook-Pro-10A.local>
2019-04-25 23:36:51 -0700

    added createdAt, footer, and bootstrap theme

--- controllers/events.js
+++ controllers/events.js
@@ -52,6 +52,7 @@ app.get('/events/new', (req, res) => {
 app.get('/events/:id', (req, res) => {
   // Search for the event by its id that was passed in via req.params
   models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then((event) => {
+
     // If the id is for a valid event, show it
     res.render('events-show', { event: event })
   }).catch((err) => {
